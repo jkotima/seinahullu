7892b5aff4cb1974db4f0a0cb99f73d7130f2c22
Juuso Kotimäki <jkotima@gmail.com>
2022-02-22 11:35:33 +0200

    init maven

---
+++ src/main/java/com/jkotima/seinahullu/SeinahulluApplication.java
@@ -0,0 +1,13 @@
+package com.jkotima.seinahullu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeinahulluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeinahulluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkotima/seinahullu/SeinahulluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkotima.seinahullu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeinahulluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
