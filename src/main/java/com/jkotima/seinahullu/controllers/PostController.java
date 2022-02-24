fbe2a12377a31c1df1dd8c305e1d5e7875f7e385
Juuso Kotimäki <jkotima@gmail.com>
2022-02-24 12:39:53 +0200

    get post by id

--- src/main/java/com/jkotima/seinahullu/controllers/PostController.java
+++ src/main/java/com/jkotima/seinahullu/controllers/PostController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,5 +45,10 @@ public class PostController {
     
     postRepository.save(post);
     return ResponseEntity.ok(new MessageResponse("Posted successfully!"));
+  }
+
+  @GetMapping("/{id}")
+  public Post getUser(@PathVariable long id) {
+    return postRepository.findById(id).get();
   }
 }
