180700784ac0f7ef451a2b425e6f250f3386bf98
Juuso Kotimäki <jkotima@gmail.com>
2022-03-04 14:29:35 +0200

    test: normal user cannot change roles

--- src/test/java/com/jkotima/seinahullu/controllers/UserControllerTest.java
+++ src/test/java/com/jkotima/seinahullu/controllers/UserControllerTest.java
@@ -101,7 +101,7 @@ public class UserControllerTest {
         "email exists in db");
     accessTokenFor("newusernamefromadmin", "newpasswordfromadmin123!");
   }
-  
+
   @Test
   public void normalUserCantReplaceOtherUsersData() throws Exception {
     String auth = "Bearer " + accessTokenFor("normalUser", "password123!");
@@ -117,7 +117,7 @@ public class UserControllerTest {
             "newemail@email.com")))
         .andExpect(status().is4xxClientError());
   }
-  
+
   @Test
   public void adminCanChangeRoles() throws Exception {
     String auth = "Bearer " + accessTokenFor("adminUser", "password123!");
@@ -143,10 +143,28 @@ public class UserControllerTest {
             .anyMatch(r -> r.getName().equals(ERole.ROLE_ADMIN)),
         "User has a role admin");
   }
-  
-  @Test
-  public void normalUserCantChangeRoles() throws Exception {}
 
   @Test
-  public void moderatorUserCantChangeRoles() throws Exception {}
+  public void normalUserCantChangeRoles() throws Exception {
+    String auth = "Bearer " + accessTokenFor("normalUser", "password123!");
+    Long id = userRepository.findByUsername("normalUser").get().getId();
+
+    mockMvc.perform(put("/api/users/" + id)
+        .header("Authorization", auth)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(String.format(
+            "{\"username\":\"%s\",\"password\":\"%s\",\"email\":\"%s\",\"roles\":%s}",
+            "newusernamefromadmin",
+            "newpasswordfromadmin123!",
+            "newemailfromadmin@email.com",
+            "[\"admin\"]")));
+    Assert.isTrue(
+        !userRepository
+            .findById(id)
+            .get()
+            .getRoles()
+            .stream()
+            .anyMatch(r -> r.getName().equals(ERole.ROLE_ADMIN)),
+        "User does not have a role admin");
+  }
 }
