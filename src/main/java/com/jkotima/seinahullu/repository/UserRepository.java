b145eac4eeb557c802bb707068ebedeed18801ff
Juuso Kotimäki <jkotima@gmail.com>
2022-03-04 14:22:01 +0200

    fidbyid that also fetches roles

--- src/main/java/com/jkotima/seinahullu/repository/UserRepository.java
+++ src/main/java/com/jkotima/seinahullu/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.jkotima.seinahullu.repository;
 
 import java.util.Optional;
-
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +10,10 @@ import com.jkotima.seinahullu.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
-
+  
+  @EntityGraph(attributePaths = {"roles"})
+  Optional<User> findById(Long id);
+  
   Boolean existsByUsername(String username);
 
   Boolean existsByEmail(String email);
